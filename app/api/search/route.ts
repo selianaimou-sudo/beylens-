8b713f91871ac2102e1cd07c2158154579c5b61d
selianaimou-sudo <selianaimou@gmail.com>
2026-05-08 10:47:02 +0200

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -2,10 +2,15 @@ import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
+
   const q = searchParams.get("q") || "iphone";
+  const gl = searchParams.get("gl") || "de";
+  const hl = searchParams.get("hl") || "en";
 
   const res = await fetch(
-    `https://serpapi.com/search.json?engine=google_shopping&q=${encodeURIComponent(q)}&gl=de&hl=en&api_key=${process.env.SERPAPI_API_KEY}`
+    `https://serpapi.com/search.json?engine=google_shopping&q=${encodeURIComponent(
+      q
+    )}&gl=${gl}&hl=${hl}&api_key=${process.env.SERPAPI_API_KEY}`
   );
 
   const data = await res.json();
